b1fa885f85b4cacc70ee3dae2101cb3f2aa84146
Arjan de Ruijter <rra@mboutrecht.nl>
2016-11-21 10:59:46 +0100

    registratieform

--- wp-content/plugins/registrationform/registrationfrom.php
+++ wp-content/plugins/registrationform/registrationfrom.php
@@ -9,129 +9,117 @@
    */
    ob_start();
 
+   function sanitize($text)
+   {
+      
+      // Haalt spaties, \n, returns, enz.. links en rechts weg
+      $text = trim($text);
+
+      // Verwijdert html en php tags
+      $text = strip_tags($text);
+
+      // escaped kritische tekens zoals ' en "
+      $text = addslashes($text);
+
+      // of gebruik...
+      //$text = mysqli_real_escape_string($conn, $text);
+      return $text;
+   }
+
    function registrationform()
    {
       global $wpdb;
 
       if (isset($_POST["submit"]))
       {
-            // Maak contact met de mysql-server en database 
-      include("./connect_db.php");
+            
+            // Stel de juiste tijdzone in voor het bepalen van de tijd
+            date_default_timezone_set("Europe/Amsterdam");
 
-      // Maak de functie sanitize() beschikbaar op deze pagina
-      include("./functions.php");
-
-      // Stel de juiste tijdzone in voor het bepalen van de tijd
-      date_default_timezone_set("Europe/Amsterdam");
-
-      // Maak een random tijdelijk password en haal dit door een sha1 hash
-      $first3OfFirstname = substr(sanitize($_POST["firstname"]), 0, 3);
-      $last4OfLastname = substr(sanitize($_POST["lastname"]), (strlen(sanitize($_POST["lastname"])) - 4), 4);
-      $date = date("d-m-Y H:i:s");
-      $tempPassword = $date." ".$first3OfFirstname." ".$last4OfLastname;
-      $tempPassword = sha1($tempPassword);
+            // Maak een random tijdelijk password en haal dit door een sha1 hash
+            $first3OfFirstname = substr(sanitize($_POST["firstname"]), 0, 3);
+            $last4OfLastname = substr(sanitize($_POST["lastname"]), (strlen(sanitize($_POST["lastname"])) - 4), 4);
+            $date = date("d-m-Y H:i:s");
+            $tempPassword = $date." ".$first3OfFirstname." ".$last4OfLastname;
+            $tempPassword = sha1($tempPassword);
 
 
-      // Maak de waarden van het $_POST() array schoon
-      $firstname = sanitize($_POST["firstname"]);
-      $infix = sanitize($_POST["infix"]);
-      $lastname = sanitize($_POST["lastname"]);
-      $email = sanitize($_POST["email"]);
+            // Maak de waarden van het $_POST() array schoon
+            $firstname = sanitize($_POST["firstname"]);
+            $infix = sanitize($_POST["infix"]);
+            $lastname = sanitize($_POST["lastname"]);
+            $email = sanitize($_POST["email"]);
+            $loginname = sanitize($_POST["loginname"]);
 
-      // Maak de selectiequery om te controleren of het emailadres al bestaat in de database
-      $sql = "SELECT * FROM `users` WHERE `email` = '".$_POST["email"]."'";
+            // Maak de selectiequery om te controleren of het emailadres al bestaat in de database
+            $user = get_user_by("email", $_POST["email"]);
 
-      $result = mysqli_query($conn, $sql);
+            //var_dump($user); 
 
-      if (mysqli_num_rows($result) == 0)
-      {
+            if (!$user)
+            {
+                  // Voeg de gebruiker toe aan de database
+                  $userdata = array(user_pass => $tempPassword,
+                                    user_login => $loginname,
+                                    user_nicename => $loginname,
+                                    first_name => $firstname,
+                                    last_name => $lastname,
+                                    user_email => $email,
+                                    role => "subscriber");
 
-        // Maak de insert query..
-        $sql = "INSERT INTO `users` (`id`,
-                                    `firstname`,
-                                    `infix`,
-                                    `lastname`,
-                                    `email`,
-                                    `password`,
-                                    `activate`)
-                VALUES             (NULL,
-                                    '".$firstname."',
-                                    '".$infix."',
-                                    '".$lastname."',
-                                    '".$email."',
-                                    '".$tempPassword."',
-                                    'false')";
+                  $last_id = wp_insert_user($userdata);        
         
-        // Vuur de query af op de database via de verbinding $conn
-        $result = mysqli_query($conn, $sql);
-      
-        // Vraag het id op van het aangemaakt record
-        $last_id = mysqli_insert_id($conn);
-
-        
-        // Als de query correct is ontvangen en uitgevoerd.
-        if ($result)
-        {
-          $emailaddress = $_POST["email"];
-          $subject = "Activatie account";
+                  //var_dump($result); exit();
+                 
+                  // Als de query correct is ontvangen en uitgevoerd.
+                  if (!empty($last_id))
+                  {
+                        $emailaddress = $email;
+                        $subject = "Activatie account";        
+                   
+                        $messageHtml = "<!DOCTYPE html>
+                                          <html>
+                                          <head>
+                                                <title>Page Title</title>
+                                                <style>
+                                                body
+                                                {
+                                                      font-family: Verdana, Arial;
+                                                      font-size: 1em;
+                                                      color: rgb(30, 30, 30);
+                                                }
+                                                </style>
+                                          </head>
+                                          <body>
+                                          <h3>Beste ".$firstname." ".$infix." ".$lastname.",</h3>".
+                                                "<p>Bedankt voor het registreren, klik op onderstaande link<p>".
+                                                "<p><a href='http://localhost/2016-2017/am1a/groenten/index.php/activeer-uw-account/?id=".
+                                                $last_id."&pw=".$tempPassword."'>activatielink</a></p><p>om uw account te activeren</p>". 
+                                                "<p>Met vriendelijke groet,</p>". 
+                                                "<p>De beheerder van de site</p>
+                                          </body>
+                                          </html>";
           
-          /*
-          $message = "Beste ".$firstname." ".$infix." ".$lastname."\n\n".
-                      "Bedankt voor het registreren, klik op onderstaande link\n\n".
-                      "http://localhost/2016-2017/am1a/inlogregistratiesysteem/index.php?content=activate&id=".
-                      $last_id."&pw=".$tempPassword." \n\nom uw account te activeren\n\n". 
-                      "Met vriendelijke groet,\n". 
-                      "admin";
-          */
-          $messageHtml = "<!DOCTYPE html>
-                          <html>
-                            <head>
-                                <title>Page Title</title>
-                                <style>
-                                  body
-                                  {
-                                      font-family: Verdana, Arial;
-                                      font-size: 1em;
-                                      color: rgb(30, 30, 30);
-                                  }
-                                </style>
-                            </head>
-                            <body>
-                            <h3>Beste ".$firstname." ".$infix." ".$lastname.",</h3>".
-                                "<p>Bedankt voor het registreren, klik op onderstaande link<p>".
-                                "<p><a href='http://localhost/2016-2017/am1a/inlogregistratiesysteem/index.php?content=activate&id=".
-                                $last_id."&pw=".$tempPassword."'>activatielink</a></p><p>om uw account te activeren</p>". 
-                                "<p>Met vriendelijke groet,</p>". 
-                                "<p>admin</p>
-                            </body>
-                          </html>";
-          
-          $headers = "Content-Type: text/html; charset=UTF-8"."\r\n";
-          $headers .= "Cc: adruijter@fopmail.com, hans@testmail.com, frans@realmail.com"."\r\n";
-          $headers .= "Bcc: rra@mboutrecht.nl"."\r\n";
-          $headers .= "From: adruijter@gmail.com";
+                        $headers = "Content-Type: text/html; charset=UTF-8"."\r\n";
+                        $headers .= "Cc: adruijter@fopmail.com, hans@testmail.com, frans@realmail.com"."\r\n";
+                        $headers .= "Bcc: rra@mboutrecht.nl"."\r\n";
+                        $headers .= "From: adruijter@gmail.com";
 
-
-
-          mail($emailaddress, $subject, $messageHtml, $headers);
-          // Geef een succes melding en stuur door naar de homepage..
-          echo "Uw bent geregistreerd";
-          // Wacht 3 seconden en stuur door naar index.php?content=home
-          header("refresh:3; url=./index.php?content=home");
-        $userdata = array(user_pass => "geheim",
-                          user_login => $_POST["loginname"],
-                          user_nicename => $_POST["loginname"],
-                          first_name => $_POST["firstname"],
-                          last_name => $_POST["lastname"],
-                          user_email => $_POST["email"],
-                          role => "subscriber");
-
-         wp_insert_user($userdata);
-
-         $output = "Uw bent succesvol geregistreerd. U ontvangt een activatiemail";
-         header("Refresh: 4; url=http://localhost/2016-2017/am1a/groenten/");
-                  
-         return $output;
+                        mail($emailaddress, $subject, $messageHtml, $headers);
+                        
+                        // Geef een succes melding en stuur door naar de homepage..
+                        $output = "Uw bent succesvol geregistreerd. U ontvangt een activatiemail";
+                        header("Refresh: 4; url=http://localhost/2016-2017/am1a/groenten/"); 
+                        
+                        return $output;
+                   }
+            }
+            else
+            {
+                  $output = "Dit e-mailadres is al in gebruik. Kies een ander";
+                  header("Refresh: 4; url=http://localhost/2016-2017/am1a/groenten/index.php/registratie/"); 
+                  return $output;
+            }
       }
       else
       {
